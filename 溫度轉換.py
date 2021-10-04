42ca2043dcfa724a5346e43318e55b05d554c483
Herry <a0938039592@gmail.com>
2021-10-04 21:38:51 +0800

    python練習

---
+++ 溫度轉換.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from random import randint
+import random
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+c =float(input("請輸入攝氏溫度: "))
+f = c * 9  /5 + 32
+print("華氏溫度", f)
+
+
+    
+
+
+
+
+
+
